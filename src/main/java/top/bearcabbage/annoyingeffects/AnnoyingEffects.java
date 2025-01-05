e3d46d6bdc1a3266ffa6a7c9a543340ed8416b66
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2025-01-05 18:15:41 +0800

    将效果的时长/间隔写入配置文件

--- src/main/java/top/bearcabbage/annoyingeffects/AnnoyingEffects.java
+++ src/main/java/top/bearcabbage/annoyingeffects/AnnoyingEffects.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -29,7 +30,9 @@ import net.minecraft.util.math.BlockPos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.bearcabbage.annoyingeffects.effect.*;
+import top.bearcabbage.annoyingeffects.utils.ConfigReadAndWrite;
 
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -40,6 +43,7 @@ import static net.minecraft.item.Items.*;
 
 public class AnnoyingEffects implements ModInitializer {
 	public static final String MOD_ID = "annoyingeffects";
+	private static final ConfigReadAndWrite config = new ConfigReadAndWrite(FabricLoader.getInstance().getConfigDir().resolve("AnnoyingEffects.json"));
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -93,54 +97,54 @@ public class AnnoyingEffects implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		{
-			STATUS_EFFECT_MAP.put(ANOREXIA, pack(200, 200));
-			STATUS_EFFECT_MAP.put(CARROT_CURSE, pack(120, 600));
-			STATUS_EFFECT_MAP.put(CHANNELING, pack(20, 100));
-			STATUS_EFFECT_MAP.put(CHAOTIC_EXPLOSION, pack(30, 600));
-			STATUS_EFFECT_MAP.put(CHAOTIC_TELEPORTATION, pack(5, 200));
-			STATUS_EFFECT_MAP.put(CONTROLS_ALWAYS_DIG, pack(30, 300));
-			STATUS_EFFECT_MAP.put(CONTROLS_ALWAYS_JUMP, pack(60, 600));
-			STATUS_EFFECT_MAP.put(CONTROLS_CHAOTIC_USE, pack(120, 600));
-			STATUS_EFFECT_MAP.put(CONTROLS_CRAB, pack(120, 600));
-			STATUS_EFFECT_MAP.put(CONTROLS_MIRROR, pack(120, 600));
-			STATUS_EFFECT_MAP.put(CONTROLS_SHORT_REACH, pack(120, 600));
-			STATUS_EFFECT_MAP.put(CRAWLER, pack(60, 600));
-			STATUS_EFFECT_MAP.put(CREEPERPHOBIA, pack(120, 600));
-			STATUS_EFFECT_MAP.put(CURSE_OF_VANISHING, pack(50, 100));
-			STATUS_EFFECT_MAP.put(DISABLE_CRAFTING_TABLE, pack(60, 600));
-			STATUS_EFFECT_MAP.put(DISABLE_INVENTORY, pack(10, 100));
-			STATUS_EFFECT_MAP.put(DISABLE_SLEEPING, pack(120, 600));
-			STATUS_EFFECT_MAP.put(ENDERMAN_HOSTILE, pack(120, 600));
-			STATUS_EFFECT_MAP.put(HEAVINESS, pack(60, 600));
-			STATUS_EFFECT_MAP.put(HORSELESS, pack(5, 300));
-			STATUS_EFFECT_MAP.put(MISFORTUNE, pack(-1, -1));
-			STATUS_EFFECT_MAP.put(OPPRESSED, pack(10, 600));
-			STATUS_EFFECT_MAP.put(REALLY_COLD, pack(3, 120));
-			STATUS_EFFECT_MAP.put(REALLY_HOT, pack(3, 120));
-			STATUS_EFFECT_MAP.put(REPEATER, pack(120, 600));
-			STATUS_EFFECT_MAP.put(SCHIZOPHRENIA, pack(-1, -1));
-			STATUS_EFFECT_MAP.put(SLIPPY, pack(120, 600));
-			STATUS_EFFECT_MAP.put(SPIN, pack(10, 600));
-			STATUS_EFFECT_MAP.put(SWAPPING_HANDS, pack(10, 200));
-			STATUS_EFFECT_MAP.put(TARGETED, pack(120, 600));
-			STATUS_EFFECT_MAP.put(VOICELESS, pack(120, 600));
-			STATUS_EFFECT_MAP.put(VULNERABLE, pack(120, 600));
-			STATUS_EFFECT_MAP.put(WATER_FILLING, pack(-1, 15));
-
-			STATUS_EFFECT_MAP.put(BLINDNESS, pack(60, 600));
-			STATUS_EFFECT_MAP.put(DARKNESS, pack(60, 600));
-			STATUS_EFFECT_MAP.put(HUNGER, pack(60, 600));
-			STATUS_EFFECT_MAP.put(INFESTED, pack(120, 600));
-			STATUS_EFFECT_MAP.put(LEVITATION, pack(10, 200));
-			STATUS_EFFECT_MAP.put(MINING_FATIGUE, pack(60, 600));
-			STATUS_EFFECT_MAP.put(NAUSEA, pack(10, 200));
-			STATUS_EFFECT_MAP.put(SLOWNESS, pack(60, 600));
-			STATUS_EFFECT_MAP.put(WEAKNESS, pack(120, 600));
-			STATUS_EFFECT_MAP.put(WITHER, pack(10, 200));
-
-		}
+		loadStatusEffects();
+//		{
+//			STATUS_EFFECT_MAP.put(ANOREXIA, pack(200, 200));
+//			STATUS_EFFECT_MAP.put(CARROT_CURSE, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(CHANNELING, pack(20, 100));
+//			STATUS_EFFECT_MAP.put(CHAOTIC_EXPLOSION, pack(30, 600));
+//			STATUS_EFFECT_MAP.put(CHAOTIC_TELEPORTATION, pack(5, 200));
+//			STATUS_EFFECT_MAP.put(CONTROLS_ALWAYS_DIG, pack(30, 300));
+//			STATUS_EFFECT_MAP.put(CONTROLS_ALWAYS_JUMP, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(CONTROLS_CHAOTIC_USE, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(CONTROLS_CRAB, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(CONTROLS_MIRROR, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(CONTROLS_SHORT_REACH, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(CRAWLER, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(CREEPERPHOBIA, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(CURSE_OF_VANISHING, pack(50, 100));
+//			STATUS_EFFECT_MAP.put(DISABLE_CRAFTING_TABLE, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(DISABLE_INVENTORY, pack(10, 100));
+//			STATUS_EFFECT_MAP.put(DISABLE_SLEEPING, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(ENDERMAN_HOSTILE, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(HEAVINESS, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(HORSELESS, pack(5, 300));
+//			STATUS_EFFECT_MAP.put(MISFORTUNE, pack(-1, -1));
+//			STATUS_EFFECT_MAP.put(OPPRESSED, pack(10, 600));
+//			STATUS_EFFECT_MAP.put(REALLY_COLD, pack(3, 120));
+//			STATUS_EFFECT_MAP.put(REALLY_HOT, pack(3, 120));
+//			STATUS_EFFECT_MAP.put(REPEATER, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(SCHIZOPHRENIA, pack(-1, -1));
+//			STATUS_EFFECT_MAP.put(SLIPPY, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(SPIN, pack(10, 600));
+//			STATUS_EFFECT_MAP.put(SWAPPING_HANDS, pack(10, 200));
+//			STATUS_EFFECT_MAP.put(TARGETED, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(VOICELESS, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(VULNERABLE, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(WATER_FILLING, pack(-1, 15));
+//
+//			STATUS_EFFECT_MAP.put(BLINDNESS, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(DARKNESS, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(HUNGER, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(INFESTED, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(LEVITATION, pack(10, 200));
+//			STATUS_EFFECT_MAP.put(MINING_FATIGUE, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(NAUSEA, pack(10, 200));
+//			STATUS_EFFECT_MAP.put(SLOWNESS, pack(60, 600));
+//			STATUS_EFFECT_MAP.put(WEAKNESS, pack(120, 600));
+//			STATUS_EFFECT_MAP.put(WITHER, pack(10, 200));
+//
+//		}
 
 		UseItemCallback.EVENT.register((player, world, hand) -> {
 			ItemStack itemStack = player.getStackInHand(hand);
@@ -199,5 +203,55 @@ public class AnnoyingEffects implements ModInitializer {
 				}
 			}
 		});
+	}
+
+	private void loadStatusEffects() {
+		STATUS_EFFECT_MAP.put(ANOREXIA, pack(config.getDurationAndInterval("anorexia", Map.of("duration", 200, "interval", 200))));
+		STATUS_EFFECT_MAP.put(CARROT_CURSE, pack(config.getDurationAndInterval("carrot_curse", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CHANNELING, pack(config.getDurationAndInterval("channeling", Map.of("duration", 20, "interval", 100))));
+		STATUS_EFFECT_MAP.put(CHAOTIC_EXPLOSION, pack(config.getDurationAndInterval("chaotic_explosion", Map.of("duration", 30, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CHAOTIC_TELEPORTATION, pack(config.getDurationAndInterval("chaotic_teleportation", Map.of("duration", 5, "interval", 200))));
+		STATUS_EFFECT_MAP.put(CONTROLS_ALWAYS_DIG, pack(config.getDurationAndInterval("controls_always_dig", Map.of("duration", 30, "interval", 300))));
+		STATUS_EFFECT_MAP.put(CONTROLS_ALWAYS_JUMP, pack(config.getDurationAndInterval("controls_always_jump", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CONTROLS_CHAOTIC_USE, pack(config.getDurationAndInterval("controls_chaotic_use", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CONTROLS_CRAB, pack(config.getDurationAndInterval("controls_crab", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CONTROLS_MIRROR, pack(config.getDurationAndInterval("controls_mirror", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CONTROLS_SHORT_REACH, pack(config.getDurationAndInterval("controls_short_reach", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CRAWLER, pack(config.getDurationAndInterval("crawler", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CREEPERPHOBIA, pack(config.getDurationAndInterval("creeperphobia", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(CURSE_OF_VANISHING, pack(config.getDurationAndInterval("curse_of_vanishing", Map.of("duration", 50, "interval", 100))));
+		STATUS_EFFECT_MAP.put(DISABLE_CRAFTING_TABLE, pack(config.getDurationAndInterval("disable_crafting_table", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(DISABLE_INVENTORY, pack(config.getDurationAndInterval("disable_inventory", Map.of("duration", 10, "interval", 100))));
+		STATUS_EFFECT_MAP.put(DISABLE_SLEEPING, pack(config.getDurationAndInterval("disable_sleeping", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(ENDERMAN_HOSTILE, pack(config.getDurationAndInterval("enderman_hostile", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(HEAVINESS, pack(config.getDurationAndInterval("heaviness", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(HORSELESS, pack(config.getDurationAndInterval("horseless", Map.of("duration", 5, "interval", 300))));
+		STATUS_EFFECT_MAP.put(MISFORTUNE, pack(config.getDurationAndInterval("misfortune", Map.of("duration", -1, "interval", -1))));
+		STATUS_EFFECT_MAP.put(OPPRESSED, pack(config.getDurationAndInterval("oppressed", Map.of("duration", 10, "interval", 600))));
+		STATUS_EFFECT_MAP.put(REALLY_COLD, pack(config.getDurationAndInterval("really_cold", Map.of("duration", 3, "interval", 120))));
+		STATUS_EFFECT_MAP.put(REALLY_HOT, pack(config.getDurationAndInterval("really_hot", Map.of("duration", 3, "interval", 120))));
+		STATUS_EFFECT_MAP.put(REPEATER, pack(config.getDurationAndInterval("repeater", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(SCHIZOPHRENIA, pack(config.getDurationAndInterval("schizophrenia", Map.of("duration", -1, "interval", -1))));
+		STATUS_EFFECT_MAP.put(SLIPPY, pack(config.getDurationAndInterval("slippy", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(SPIN, pack(config.getDurationAndInterval("spin", Map.of("duration", 10, "interval", 600))));
+		STATUS_EFFECT_MAP.put(SWAPPING_HANDS, pack(config.getDurationAndInterval("swapping_hands", Map.of("duration", 10, "interval", 200))));
+		STATUS_EFFECT_MAP.put(TARGETED, pack(config.getDurationAndInterval("targeted", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(VOICELESS, pack(config.getDurationAndInterval("voiceless", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(VULNERABLE, pack(config.getDurationAndInterval("vulnerable", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(WATER_FILLING, pack(config.getDurationAndInterval("water_filling", Map.of("duration", -1, "interval", 15))));
+		STATUS_EFFECT_MAP.put(BLINDNESS, pack(config.getDurationAndInterval("blindness", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(DARKNESS, pack(config.getDurationAndInterval("darkness", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(HUNGER, pack(config.getDurationAndInterval("hunger", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(INFESTED, pack(config.getDurationAndInterval("infested", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(LEVITATION, pack(config.getDurationAndInterval("levitation", Map.of("duration", 10, "interval", 200))));
+		STATUS_EFFECT_MAP.put(MINING_FATIGUE, pack(config.getDurationAndInterval("mining_fatigue", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(NAUSEA, pack(config.getDurationAndInterval("nausea", Map.of("duration", 10, "interval", 200))));
+		STATUS_EFFECT_MAP.put(SLOWNESS, pack(config.getDurationAndInterval("slowness", Map.of("duration", 60, "interval", 600))));
+		STATUS_EFFECT_MAP.put(WEAKNESS, pack(config.getDurationAndInterval("weakness", Map.of("duration", 120, "interval", 600))));
+		STATUS_EFFECT_MAP.put(WITHER, pack(config.getDurationAndInterval("wither", Map.of("duration", 10, "interval", 200))));
+	}
+
+	private int pack(Map<String, Integer> durationAndInterval) {
+		return (durationAndInterval.get("duration") << 16) + durationAndInterval.get("interval");
 	}
 }
---
+++ src/main/java/top/bearcabbage/annoyingeffects/utils/ConfigReadAndWrite.java
@@ -0,0 +1,89 @@
+package top.bearcabbage.annoyingeffects.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.mojang.text2speech.Narrator.LOGGER;
+
+public class ConfigReadAndWrite {
+    private final Path filePath;
+    private JsonObject jsonObject;
+    private final Gson gson;
+
+    public ConfigReadAndWrite(Path filePath) {
+        this.filePath = filePath;
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+        try {
+            if (Files.notExists(filePath.getParent())) {
+                Files.createDirectories(filePath.getParent());
+            }
+            if (Files.notExists(filePath)) {
+                Files.createFile(filePath);
+                try (FileWriter writer = new FileWriter(filePath.toFile())) {
+                    writer.write("{}");
+                }
+            }
+
+        } catch (IOException e) {
+            LOGGER.error(e.toString());
+        }
+        load();
+    }
+
+    public void load() {
+        try (FileReader reader = new FileReader(filePath.toFile())) {
+            this.jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+        } catch (IOException e) {
+            this.jsonObject = new JsonObject();
+        }
+    }
+
+    public void save() {
+        try (FileWriter writer = new FileWriter(filePath.toFile())) {
+            gson.toJson(jsonObject, writer);
+        } catch (IOException e) {
+            LOGGER.error(e.toString());
+        }
+    }
+
+    public <T> void set(String key, T value) {
+        jsonObject.add(key, gson.toJsonTree(value));
+    }
+
+    public <T> T get(String key, Class<T> clazz) {
+        return gson.fromJson(jsonObject.get(key), clazz);
+    }
+
+    public <T> T get(String key, Class<T> clazz, T defaultValue) {
+        T value = gson.fromJson(jsonObject.get(key), clazz);
+        if (value == null) {
+            set(key, defaultValue);
+            save();
+        }
+        return value != null ? value : defaultValue;
+    }
+
+    public Map<String, Integer> getDurationAndInterval(String key, Map<String, Integer> defaultValue) {
+        JsonObject value = jsonObject.getAsJsonObject(key);
+        if (value == null) {
+            set(key, defaultValue);
+            save();
+            return defaultValue;
+        }
+        Map<String, Integer> result = new HashMap<>();
+        result.put("duration", value.get("duration").getAsInt());
+        result.put("interval", value.get("interval").getAsInt());
+        return result;
+    }
+    
+}
